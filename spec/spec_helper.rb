0c9a917a6246b0501334bfe11353b906984b91b4
Khaled S Momani <khaledmomani4@gmail.com>
2020-08-19 02:14:42 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,7 @@ RSpec.configure do |config|
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
     config.failure_color = :magenta
-    config.
+    config.success_color = :cyan
     # Prevents you from mocking or stubbing a method that does not exist on
     # a real object. This is generally recommended, and will default to
     # `true` in RSpec 4.
